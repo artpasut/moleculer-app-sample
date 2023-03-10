b9aae51a8538cc3cd460c20b944c03fa0454d538
Pasut Ratsameejun <45454527+artpasut@users.noreply.github.com>
2023-03-11 00:54:49 +0700

    feature: add template for deploy-service script with value override (#8)
    * feature: add template for deploy-service script with value override
    
    * fix: parameter store name ref index after split

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,9 +1,22 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 data "template_file" "user_data" {
   template = file("${path.module}/scripts/user_data.sh")
 }
 
 data "template_file" "cloud_init" {
   template = file("${path.module}/scripts/cloud-init.yml")
+}
+
+data "template_file" "deploy_service" {
+  template = file("${path.module}/scripts/deploy-service.sh")
+  vars = {
+    aws_region         = data.aws_region.current.name
+    aws_account_id     = data.aws_caller_identity.current.account_id
+    ssm_parameter_name = split("/", aws_ssm_parameter.service.arn)[1]
+  }
 }
 
 data "template_cloudinit_config" "user_data" {
--- terraform/parameter-store.tf
+++ terraform/parameter-store.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "service" {
-  name  = format("/%s/services", var.name)
+  name  = format("%s-services", var.name)
   type  = "String"
   value = format("NAMESPACE=\nLOGGER=true\nLOGLEVEL=info\nSERVICEDIR=services\nTRANSPORTER=nats://%s:4222", module.ec2_nats_service.private_dns)
 }
