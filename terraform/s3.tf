7b29b1a05bda6a5d17450d3390955d8526870ba7
artpasut <pasut67@gmail.com>
2023-03-10 22:04:40 +0700

    fix: terraform s3 script storage

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,14 +13,14 @@ resource "aws_s3_bucket_acl" "scripts" {
 }
 
 resource "aws_s3_object" "script_service" {
-  bucket = aws_s3_bucket.scripts.name
+  bucket = aws_s3_bucket.scripts.id
   key    = "scripts/deploy-service.sh"
   source = "./scripts/deploy-service.sh"
   etag   = filemd5("./scripts/deploy-service.sh")
 }
 
 resource "aws_s3_object" "script_nats" {
-  bucket = aws_s3_bucket.scripts.name
+  bucket = aws_s3_bucket.scripts.id
   key    = "scripts/deploy-nats.sh"
   source = "./scripts/deploy-nats.sh"
   etag   = filemd5("./scripts/deploy-nats.sh")
